d56e59bd064d4fcdd06f86ce5f9a792c351dbf8f
vaishnavp2000 <vaishnavp860@gmail.com>
2025-09-10 15:57:06 +0530

    created the model of order and order products

--- app/Models/OrderProduct.php
+++ app/Models/OrderProduct.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderProduct extends Model
 {
-    //
+    protected $fillable=[
+        'order_id',
+        'product_id',
+        'row_id',
+        'sku_id',
+        'sku_alt',
+        'upc',
+        'style_number',
+        'description',
+        'size',
+        'qty',
+        'is_taxable',
+        'amount',
+        'unit_price',
+        'warehouse_id',
+        'shopify_order_id',
+        'shopify_order_name',
+        'shopify_sku',
+        'qty_picked',
+        'qty_cancelled',
+        'qty_shipped',
+        'date_due',
+        'shopify_line_item_id',
+        'shopify_title',
+        'shopify_quantity',
+        'shopify_current_quantity',
+        'shopify_variant_id',
+        'shopify_variant_title',
+        'shopify_fulfillment_order_id'
+    ];
 }
