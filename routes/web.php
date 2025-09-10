372c3971072e61132203c814a16086c98d5fd336
vaishnavp2000 <vaishnavp860@gmail.com>
2025-09-10 13:36:51 +0530

    update the route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ Route::prefix('admin')->name('admin.')->namespace('App\Http\Controllers\Admin')-
 
         // HomeController 
         Route::get('/', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('dashboard');
+        Route::resource('product', App\Http\Controllers\Admin\ProductController::class);
+        Route::post('fetch-product', [App\Http\Controllers\Admin\ProductController::class, 'fetchProducts'])->name('fetch-product');
+        Route::post('create-am-products', [App\Http\Controllers\Admin\ProductController::class, 'createAmProducts'])->name('create-am-products');
 
     
     });
