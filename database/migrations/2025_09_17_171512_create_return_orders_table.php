975e03c47dba0b0fabf77a63907a7da26755b9b3
vaishnavp2000 <vaishnavp860@gmail.com>
2025-09-18 09:32:31 +0530

    added themigration for  am return table

---
+++ database/migrations/2025_09_17_171512_create_return_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('return_orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('am_order_id')->nullable();
+            $table->string('shopify_order_id')->nullable();
+            $table->string('return_authorization_id')->nullable();
+            $table->string('credit_memo_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('return_orders');
+    }
+};
