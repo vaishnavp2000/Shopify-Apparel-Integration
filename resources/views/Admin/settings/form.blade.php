0a8885ca686da78fde2a79df79f12835dec40915
vaishnavp2000 <vaishnavp860@gmail.com>
2025-09-18 16:21:21 +0530

    modified the settings form

--- resources/views/Admin/settings/form.blade.php
+++ resources/views/Admin/settings/form.blade.php
@@ -1,10 +1,13 @@
 @extends('admin.layouts.app')
+
+
 @section('page-title', 'Settings')
 @section('head')
-
+<link rel="stylesheet" href="{{ url("libs/select2/css/select2.min.css") }}" type="text/css">
 @endsection
 @section('content')
-<div class="mb-4">
+    
+    <div class="mb-4">
         <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item">
@@ -15,108 +18,85 @@
                 <li class="breadcrumb-item active" aria-current="page">Settings</li>
             </ol>
         </nav>
-</div>
-    
-<div class="row flex-column-reverse flex-md-row">
-    <div class="col-md-12">
-        <div class="tab-content" id="myTabContent">
-            <div class="tab-pane fade show active" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                <div class="mb-4">
-                    <div class="card mb-4">
-                        <form id="settings_form" action="{{route('admin.settings.store')}}" method="post">
+    </div>
+
+    <div class="row flex-column-reverse flex-md-row">
+        <div class="col-md-12">
+            <div class="card mb-4">
+                    <div class="card-body">
+                        <h6 class="card-title mb-4">{{ $platform }} Settings</h6>
+                        <form method="POST" action="{{route('admin.settings.store')}}">
                             @csrf
-                            <div class="card-body">
-                                <div class="row">
-                                    @foreach($settings as $setting)
+                            <div class="row">
+                                @foreach ($settingData as  $setting)
                                     <div class="col-md-6">
                                         <div class="mb-3">
                                             <label class="form-label">{{ $setting->title }}</label>
-                                            @if($setting->field_type == 'select')
-                                                <select name="{{ $setting->code }}" class="form-select">
-                                                    @if(!empty($setting->dataset))
-                                                        @foreach(json_decode($setting->dataset) as $dataset)
-                                                            <option value="{{ $dataset->id }}" {{ ($dataset->id==$setting->value) ? 'selected' : '' }}>{{ $dataset->name }}</option>
+                                            @if($setting->field_type=='select')
+                                              <select name="{{ $setting->code }}" class="form-select">
+                                                @if(!empty($setting->data_source)&&isset($dataSets[$setting->code]))
+
+                                                    @foreach($dataSets[$setting->code] as $key=> $dataset)
+                                                            <option value="{{ $key }}" {{ ($dataset==$setting->value) ? 'selected' : '' }}>{{ $dataset }}</option>
                                                         @endforeach
-                                                    @endif
-                                                </select>
-
-                                                @elseif($setting->field_type == 'textarea')
-                                                    <textarea class="form-control" rows="4" name="{{ $setting->code }}">{{ $setting->value }}</textarea>
-
-                                                @elseif($setting->field_type == 'password')
-                                                    <div class="input-group position-relative">
-                                                        <input autocomplete="off" type="password" name="{{ $setting->code }}" class="form-control password-field"
-                                                            value="{{ $setting->value }}" placeholder="{{ $setting->placeholder }}">
-                                                        <span class="fa fa-fw toggle-password fa-eye-slash position-absolute"
-                                                            style="top: 50%; right: 15px; transform: translateY(-50%); cursor: pointer;"></span>
-                                                    </div>
-
-                                                @else
-                                                    <input autocomplete="off" type="{{ $setting->field_type }}" name="{{ $setting->code }}" class="form-control"
-                                                        value="{{ $setting->value }}" placeholder="{{ $setting->placeholder }}">
+                                                
+                                                @elseif(!empty($setting->dataset))
+                                                  
+                                                        @foreach(json_decode($setting->dataset) as $dataset)
+                                                        <option value="{{ $dataset->id }}" {{ ($dataset->id==$setting->value) ? 'selected' : '' }}>{{ $dataset->name }}</option>
+                                                        @endforeach
+                                                   
                                                 @endif
+                                                 </select>
+                                            @elseif($setting->field_type=='multiple')
+                                                @php $dataSelected=explode(',',$setting->value);@endphp
+                                            <select name="{{ $setting->code }}[]" class="select2" multiple>
+                                                @foreach(json_decode($setting->dataset) as $dataset)
+                                                <option value="{{ $dataset->id }}" {{in_array($dataset->id, $dataSelected) ? 'selected' : '' }}>{{ $dataset->name }}</option>
+                                                @endforeach
+                                            </select>
+                                            @elseif($setting->field_type=='textarea')
+                                                <textarea class="form-control" rows="4" name="{{ $setting->code }}">{{ $setting->value }}</textarea>
+                                            @else 
+                                                <input type="{{ $setting->field_type }}" name="{{ $setting->code }}" class="form-control"
+                                                value="{{ $setting->value }}" placeholder="{{ $setting->placeholder }}">
+                                            @endif
                                         </div>
                                     </div>
-                                    @endforeach
-                                    <div class="col-md-12">
+                                @endforeach
+                                <div class="col-md-12">
+                                    <div class="mb-3">
                                         <input type="hidden" value="{{ $platform }}" name="platform">
-                                        <div class="mb-3">
-                                          
-                                            <button type="submit" class="btn btn-primary mb-3 pull-right settings_btn">Save</button>
-                                           
-                                        </div>
+                                        <button type="submit" class="btn btn-primary mb-3 pull-right">Save</button>
+                                        @if($platform == 'Mail')
+                                            <button type="button" id="sendTestMailBtn" class="btn btn-primary mb-3 pull-right" style="margin-right: 10px;">
+                                                Test Mail
+                                            </button>
+                                        @endif
+
                                     </div>
                                 </div>
                             </div>
                         </form>
-
                     </div>
-
                 </div>
-
             </div>
-
-        </div>
-
+           
     </div>
-
-</div>
+  
 @endsection
 @section('script')
+
+<script src="{{ url('libs/select2/js/select2.min.js') }}"></script>
 <script>
-    $(document).ready(function(){
-        $('#settings_form').on('submit',function(){
-            $('.settings_btn').text('Saving...');
-        });
-    });
+    $('.select2').select2({ placeholder: 'Select' });
 
     @if(Session::has('success_message'))
-        $(document).ready( function () {
-            Swal.fire({
-                icon: 'success',
-                title: '{{ Session::get('success_message') }}',
-                // text: 'we will notify through email once the adjustment is complete',
-                footer: ''
-            })
-        })
-    @endif
-    @if(Session::has('error_message'))
-        $(document).ready( function () {
-            Swal.fire({
-                icon: 'error',
-                title: '{{ Session::get('error_message') }}',
-                // text: 'we will notify through email once the adjustment is complete',
-                footer: ''
-            })
-        })
-    @endif
-$('.toggle-password').click(function() {
-            var show = $(this).hasClass('fa-eye-slash');
-            $(this).toggleClass('fa-eye-slash');
-            $(this).toggleClass('fa-eye');
-            $(this).toggleClass('text-primary');
-            $(this).parent().find('.form-control').attr('type', show ? 'text' : 'password');
+        Swal.fire({
+            icon: 'success',
+            title: '{{ Session::get('success_message') }}'
         });
+    @endif
 </script>
-@endsection
 
+@endsection
